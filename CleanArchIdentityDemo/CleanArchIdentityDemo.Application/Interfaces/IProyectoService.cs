8620d70cc267430ccff25a84c3c2e8a5b48ccd55
EliecerMS <ejmendozasoto@gmail.com>
2025-09-24 16:55:19 -0600

    Organizacion con comentarios en IProyectoService para crear metodos para HU de modulo de proyectos

--- CleanArchIdentityDemo/CleanArchIdentityDemo.Application/Interfaces/IProyectoService.cs
+++ CleanArchIdentityDemo/CleanArchIdentityDemo.Application/Interfaces/IProyectoService.cs
@@ -10,7 +10,69 @@ namespace CleanArchIdentityDemo.Application.Interfaces
         Task ActualizarProyectoAsync(ProyectoDto Proyecto);
         Task EliminarProyectoAsync(string CodigoProyecto);
 
-        //Cualquier otros metodos relacionado al modulo de proyecto abajo
 
+        //metodo para cambiar el estado de un proyecto
+
+
+        //metodo para mostrar personal asignado del proyecto
+
+
+        //metodo para asignar nuevo personal a un proyecto
+
+
+        //metodo para reasignar nuevo personal a un proyecto
+
+
+        //metodo para eliminar personal de un proyecto
+
+
+
+
+        //metodo para mostrar tareas de un proyecto
+
+
+        //metodo para crear tarea de un proyecto
+
+
+        //metodo para editar tarea de un proyecto
+
+
+        //metodo para eliminar tarea de un proyecto
+
+
+        //metodo para mostrar notas de avance de un proyecto
+
+
+        //metodo para crear nota de avance de un proyecto
+
+
+        //metodo para editar nota de avance de un proyecto
+
+
+        //metodo para destacar nota de avance de un proyecto
+
+
+
+
+
+        //metodo para mostrar incidentes de un proyecto
+
+
+        //metodo para registrar incidente de un proyecto
+
+
+        //metodo para editar incidente de un proyecto
+
+
+        //metodo para cerrar incidente de un proyecto
+
+
+        //metodo para mostrar las solicitudes de material del proyecto enviadas a bodega central
+
+
+        // metotodo para editar la solicitud de material enviada a bodega central
+
+
+        //metodo para eliminar solicitud de material enviada a bodega central
     }
 }
