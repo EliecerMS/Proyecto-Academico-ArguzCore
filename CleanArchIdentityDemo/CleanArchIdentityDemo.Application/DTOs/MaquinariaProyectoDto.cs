fed5800b0191372bf0402f53fc03fea301c8bcf7
Checho0912 <117374283+Checho0912@users.noreply.github.com>
2025-10-17 20:59:41 -0600

    CRUD de Equipos/Maqinaria

---
+++ CleanArchIdentityDemo/CleanArchIdentityDemo.Application/DTOs/MaquinariaProyectoDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CleanArchIdentityDemo.Application.DTOs
+{
+    public class MaquinariaProyectoDto
+    {
+        public int MaquinariaId { get; set; }
+
+        public int ProyectoId { get; set; }
+
+        public DateTime FechaAsignacion { get; set; }
+
+    }
+}
