14e4e7c50fb4e1a3d8c241f9af2a9c62f460d184
Jafeth30 <jafethgz3099@gmail.com>
2025-10-29 17:32:50 -0600

    Ejecutar gastos , editar , elimanar gastos

---
+++ CleanArchIdentityDemo/CleanArchIdentityDemo.Application/DTOs/CostoEjecutadoDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchIdentityDemo.Application.DTOs
+{
+    public class CostoEjecutadoDto
+    {
+        public int IdCosto { get; set; }
+
+        public int ProyectoId { get; set; }
+
+        public string CategoriaGasto { get; set; } = string.Empty;
+
+        public decimal Monto { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public string Descripcion { get; set; } = string.Empty;
+
+        public string RutaComprobante { get; set; } = string.Empty;
+
+        public string NombreProyecto { get; set; } = string.Empty;
+
+        public bool ArchivoActualizado { get; set; } = false;
+    }
+}
